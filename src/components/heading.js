0ecc8a805dee8196ed651cde8f0d2ca212025d19
Nick Fehlinger <nfehlinger@gmail.com>
2020-10-09 01:58:33 -0400

    create heading component

---
+++ src/components/heading.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { Link, useStaticQuery, graphql } from "gatsby"
+import headingStyles from "./heading.module.css"
+
+export default function Heading() {
+    let data = useStaticQuery(
+    graphql`
+        {
+            allKontentItemHeading {
+                edges {
+                    node {
+                        elements {
+                            title {
+                                value
+                            }
+                            subtitle {
+                                value
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    `
+    )
+    let headingData = data.allKontentItemHeading.edges[0].node.elements;
+    return(
+        <div className={headingStyles.heading}>
+            <Link>
+                <h1>{headingData.title.value}</h1>
+                <h2>{headingData.subtitle.value}</h2>
+            </Link>
+        </div>
+    )
+}
