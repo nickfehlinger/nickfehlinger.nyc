f5c97c30d8ae4f2ad1f92eba1d8110a3605bf4fc
Nick Fehlinger <nfehlinger@gmail.com>
2020-10-09 02:00:40 -0400

    remove unused imports

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link, useStaticQuery, graphql } from "gatsby"
 import layoutStyles from "./layout.module.css"
 import Heading from "./heading";
 import SideNav from "./sidenav";
