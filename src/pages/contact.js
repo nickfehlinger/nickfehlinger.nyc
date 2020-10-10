ce64507c727a51f2002cb4dc594f3e44591d0489
Nick Fehlinger <nfehlinger@gmail.com>
2020-10-10 18:30:56 -0400

    it's literally just the select field value not coming through

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,14 +12,14 @@ export default function Contact (){
 				<form name="contact" method="POST" data-netlify="true" action="/thankyou">
 					<input type="hidden" name="form-name" value="contact" />
 					<p>
-						<label for="Name">Name: </label><input type="text" name="Name" />
+						<label for="Name">Name: </label><input type="text" name="Name" required />
 					</p>
 					<p>
-						<label for="Email">Email: </label><input type="Email" name="Email" />
+						<label for="Email">Email: </label><input type="Email" name="Email" required/>
 					</p>
 					<p>
-						<label for="Reason">Reason For Contact: </label><select name="Reason[]">
-							<option disabled>Choose One</option>
+						<label for="Reason">Reason For Contact: </label><select name="Reason[]" required>
+							<option disabled selected>Choose One</option>
 							<option value="Hire-FT">Hiring (Full Time)</option>
 							<option value="Hire-Freelance">Hiring (Freelance)</option>
 							<option value="General">General Inquiry</option>
@@ -27,7 +27,7 @@ export default function Contact (){
 						</select>
 					</p>
 					<p>
-						<label for="Message">Message: </label><textarea name="Message"></textarea>
+						<label for="Message">Message: </label><textarea name="Message" required></textarea>
 					</p>
 					<p className="submit"><button type="submit">Send</button></p>
 				</form>
