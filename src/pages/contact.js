a119603bbcee5d100ed42fd8debb5fe9686d8fc5
Nick Fehlinger <nfehlinger@gmail.com>
2020-10-12 01:27:47 -0400

    test

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,8 +9,8 @@ export default function Contact (){
 				<p>Feel free to drop me a line! I can't wait to hear from you!</p>
 			</div>
 			<div className="contact">
-				<form name="contact" method="POST" data-netlify="true" action="/thankyou">
-					<input type="hidden" name="form-name" value="contact" />
+				<form name="contact2" method="POST" data-netlify="true" action="/thankyou">
+					<input type="hidden" name="form-name" value="contact2" />
 					<p>
 						<label for="Name">Name: </label><input type="text" name="Name" required />
 					</p>
@@ -18,7 +18,13 @@ export default function Contact (){
 						<label for="Email">Email: </label><input type="Email" name="Email" required/>
 					</p>
 					<p>
-						<label for="Subject">Subject: </label><input type="text" name="Subject" required/>
+						<label for="Reason">Reason For Contact: </label><select name="Reason" required>
+							<option disabled selected>Choose One</option>
+							<option value="Hire-FT">Hiring (Full Time)</option>
+							<option value="Hire-Freelance">Hiring (Freelance)</option>
+							<option value="General">General Inquiry</option>
+							<option value="Other">Other</option>
+						</select>
 					</p>
 					<p>
 						<label for="Message">Message: </label><textarea name="Message" required></textarea>
