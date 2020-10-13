dba2e5a03badf9fe9262cf7996fd40150e3c392f
Nick Fehlinger <nfehlinger@gmail.com>
2020-10-13 18:25:37 -0400

    debugging

--- src/components/sidenav.js
+++ src/components/sidenav.js
@@ -51,7 +51,7 @@ export default function SideNav(){
     })
     console.log(sideNavDisplay)
     return (
-      <ul className={sidenavStyles.navList} id="sideNav" style={{display: sideNavDisplay}}>
+      <ul className={sidenavStyles.navList} id="sideNav" style={{display: `${window.innerWidth < 480 ? "none" : "block"}`}}>
             {navValues.map((navLink) => {
               let link = `/${navLink.node.fields.slug}`,
               value = navLink.node.elements.page_name.value;
