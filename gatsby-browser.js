3a000b2541ec642b394ea52c1731f8a4916c855c
Nick Fehlinger <nfehlinger@gmail.com>
2020-10-21 01:10:15 -0400

    debugging

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,6 @@
 import "./src/styles/global.css"
 
-export const onRouteUpdate = () => {
+export const onClientEntry = () => {
 	window.onload = () => { 
       let sideNavDisplay = window.innerWidth < 480 ? "none" : "block";
       document.getElementById("sideNav").style.display = `${sideNavDisplay}`;
