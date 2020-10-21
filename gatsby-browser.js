f27ee9217f9297e0cbcc24c1354a930674295f45
Nick Fehlinger <nfehlinger@gmail.com>
2020-10-21 02:17:37 -0400

    debug

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,11 @@
 import "./src/styles/global.css"
 
-export const onClientEntry = () => {
-	window.onload = () => { 
-      let sideNavDisplay = window.innerWidth < 480 ? "none" : "block";
-      document.getElementById("sideNav").style.display = `${sideNavDisplay}`;
-	}
-  }
+// export const onClientEntry = () => {
+// 	window.onload = () => { 
+//       let sideNavDisplay = window.innerWidth < 480 ? "none" : "block";
+//       document.getElementById("sideNav").style.display = `${sideNavDisplay}`;
+// 	}
+//   }
 
   export const onRouteUpdate = ({ location, prevLocation }) => {
 	console.log('new pathname', location.pathname)
