aa923af560e865993a7ca411b80a268d032abe97
Nick Fehlinger <nfehlinger@gmail.com>
2020-10-21 03:33:47 -0400

    clean up

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
 import "./src/styles/global.css"
 
 
-  export const onRouteUpdate = ({ location, prevLocation }) => {
+  export const onRouteUpdate = () => {
 	let sideNavDisplay = window.innerWidth < 480 ? "none" : "block";
     document.getElementById("sideNav").style.display = `${sideNavDisplay}`
   }
