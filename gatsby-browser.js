03836f56b1fe5c408d6116aa1ce9400d0198c4d0
Nick Fehlinger <nfehlinger@gmail.com>
2020-10-23 17:55:15 -0400

    update mobile breakpoint

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,6 @@ import "./src/styles/global.css"
 
 
   export const onRouteUpdate = () => {
-	let sideNavDisplay = window.innerWidth < 480 ? "none" : "block";
+	let sideNavDisplay = window.innerWidth < 960 ? "none" : "block";
     document.getElementById("sideNav").style.display = `${sideNavDisplay}`
   }
--- src/components/sidenav.js
+++ src/components/sidenav.js
@@ -4,7 +4,7 @@ import sidenavStyles from "./sidenav.module.css"
 import { window, document, exists } from 'browser-monads';
 
 let hideNav = function(){
-  if(window.innerWidth < 480){
+  if(window.innerWidth < 960){
     document.getElementById("sideNav").style.display = "none"
   }
 }
@@ -42,7 +42,7 @@ export default function SideNav(){
     let navValues = data.allKontentItemPage.edges
 
     window.addEventListener('resize', function(){
-      let sideNavDisplay = window.innerWidth < 480 ? "none" : "block";
+      let sideNavDisplay = window.innerWidth < 960 ? "none" : "block";
       document.getElementById("sideNav").style.display = `${sideNavDisplay}`;
     })
 
